59e197be9f6e9ae7f089bf3639d965729487274b
IagoWag <iagowagner12@gmail.com>
2022-07-11 04:29:08 -0300

    Install styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <View>
+      <Text>sdadsa</Text>
+    </View>
+  );
 };
 
 export default App;
