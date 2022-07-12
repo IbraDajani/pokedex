835b095db304cd16dc4f1374fdd3601a887c0081
Ibrahim Dajani <ibrahimdajani121@gmail.com>
2022-07-12 03:16:46 -0300

    Chore: Mocked API JSON on utils folder and created the pokemonDTO

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
-import styled from 'styled-components/native';
 import Icon from './components/Icon';
 
 const App: React.FC = () => {
-
   return (
     <View>
       <Icon icon="filter" activeColor="black" />
     </View>
   );
 };
-
 
 export default App;
