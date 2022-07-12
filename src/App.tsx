28d238a9dd6c48ca1733ce489c164f00e12d91ce
Ibrahim Dajani <ibrahimdajani121@gmail.com>
2022-07-12 04:15:31 -0300

    Feat: Initial structure of the internal screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './screens/Home';
+import Internal from './screens/Internal';
 
 const App: React.FC = () => {
   return <Home />;
---
+++ src/screens/Internal/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Text from '../../components/Text';
+import {Container} from './styles';
+
+const Internal: React.FC = () => {
+  return (
+    <Container>
+      <Text size={40}>Internal Screen</Text>
+    </Container>
+  );
+};
+
+export default Internal;
