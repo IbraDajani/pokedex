629975f6fbad90ada5784651ad14c4930db8e90c
NaaapZ <Leonardoestevesoliveira192@hotmail.com>
2022-07-12 05:08:37 -0300

    Add navigation

--- src/screens/Internal/index.tsx
+++ src/screens/Internal/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Text from '../../components/Text';
 import {Container} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {Pressable} from 'react-native';
 
 const Internal: React.FC = () => {
+  const {goBack} = useNavigation();
   return (
     <Container>
-      <Text size={40}>Internal Screen</Text>
+      <Pressable onPress={goBack}>
+        <Text size={40}>Internal Screen</Text>
+      </Pressable>
     </Container>
   );
 };
