ff85e84a8bb3c7408d15ae1f91bf3fe18d3d9669
IagoWag <iagowagner12@gmail.com>
2022-07-12 23:24:53 -0300

    added login screen designer

---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 20px;
+  background-color: antiquewhite;
+`;
+export const HeaderLogo = styled.View`
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+`;
+
+export const HeaderImageLogo = styled.Image`
+  width: 100%
+  height: 210px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginButton = styled.TouchableOpacity`
+  width: 250px;
+  height: 60px;
+  background-color: black;
+`;
