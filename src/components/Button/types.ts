2949ed29b5f1485b150ad2442699cf704827296b
Ibrahim Dajani <ibrahimdajani121@gmail.com>
2022-07-15 16:59:29 -0300

    Feat: Button component

---
+++ src/components/Button/types.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+import {TouchableOpacityProps} from 'react-native';
+import {NameIconTypes} from '../../constants/icons';
+
+type Mode = 'outlined' | 'contained';
+
+export interface ButtonProps extends TouchableOpacityProps {
+  children?: string | React.ReactNode;
+  color?: string;
+  mode?: Mode;
+  loading?: boolean;
+  icon?: NameIconTypes;
+  paddingHorizontal?: number;
+  paddingVectical?: number;
+}
+
+/**
+ * Styles
+ */
+
+export interface ContainerProps {
+  readonly color: string;
+  readonly borderColor: string;
+  readonly mode: Mode;
+  readonly paddingHorizontal?: number;
+  readonly paddingVectical?: number;
+}
+
+export interface TitleProps {
+  readonly color: string;
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {FlatList} from 'react-native';
 import {Container} from './styles';
