e627858613633831d209932da22801e79b979dac
IagoWag <iagowagner12@gmail.com>
2022-07-18 09:34:46 -0300

    I changed the initial route to SignedInRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import SignInRoutes from './navigation/SignInRoutes';
+import SignedInRoutes from './navigation/SignedInRoutes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <SignInRoutes />
+      <SignedInRoutes />
     </NavigationContainer>
   );
 };
